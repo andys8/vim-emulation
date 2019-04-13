64cd3ba654c5ff59820cc503cee0f6204b57a64d
andys8 <andys8@users.noreply.github.com>
2019-04-13 23:58:48 +0200

    Basic layout of editor

--- src/Main.elm
+++ src/Main.elm
@@ -1,49 +1,49 @@
-module Main exposing (..)
+module Main exposing (Model, Msg(..), init, main, update, view)
 
 import Browser
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
-
-
----- MODEL ----
+import Browser.Events
+import Element exposing (..)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Font as Font
+import Html exposing (Html)
+import Json.Decode as Decode
+import Platform.Sub as Sub
 
 
 type alias Model =
-    {}
-
-
-init : ( Model, Cmd Msg )
-init =
-    ( {}, Cmd.none )
-
-
-
----- UPDATE ----
+    { keyBuffer : List String
+    , mode : Mode
+    }
 
 
 type Msg
     = NoOp
+    | KeyDown String
 
 
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    ( model, Cmd.none )
+type Mode
+    = Normal
+    | Insert
 
 
-
----- VIEW ----
-
-
-view : Model -> Html Msg
-view model =
-    div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
-        ]
+initModel =
+    { keyBuffer = [], mode = Normal }
 
 
+init : ( Model, Cmd Msg )
+init =
+    ( initModel, Cmd.none )
 
----- PROGRAM ----
+
+sub =
+    Browser.Events.onKeyDown keyDecoder
+        |> Sub.map KeyDown
+
+
+keyDecoder : Decode.Decoder String
+keyDecoder =
+    Decode.field "key" Decode.string
 
 
 main : Program () Model Msg
@@ -52,5 +52,53 @@ main =
         { view = view
         , init = \_ -> init
         , update = update
-        , subscriptions = always Sub.none
+        , subscriptions = \_ -> sub
         }
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        KeyDown string ->
+            ( { model | keyBuffer = model.keyBuffer ++ [ string ] }, Cmd.none )
+
+        NoOp ->
+            ( model, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    Element.layout [ height fill, width fill ] <|
+        column
+            [ width fill, height fill ]
+            [ viewBufferNames
+            , viewBuffer
+            , viewAirline model
+
+            -- , viewKeyBuffer model.keyBuffer
+            ]
+
+
+viewBufferNames =
+    row [] [ text "File" ]
+
+
+viewBuffer =
+    column [ height fill, width fill ] [ text "Buffer" ]
+
+
+viewAirline model =
+    row [ alignBottom ] [ text (modeToString model.mode) ]
+
+
+viewKeyBuffer keyBuffer =
+    row [ alignBottom ] <| List.map text keyBuffer
+
+
+modeToString mode =
+    case mode of
+        Insert ->
+            "insert"
+
+        Normal ->
+            "Normal"

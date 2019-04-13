7fb590d53c847a99aa852cb08944adfd7e71b74c
andys8 <andys8@users.noreply.github.com>
2019-04-14 01:42:18 +0200

    Working lines and insert mode
    Some keys are ignored in insert mode. The lines are drawn on the left
    and the case where the width adapts to the number of digits is taken
    care of. Text can be written in insert mode and line breaks are working.

--- src/Main.elm
+++ src/Main.elm
@@ -2,17 +2,18 @@ module Main exposing (Model, Msg(..), init, main, update, view)
 
 import Browser
 import Browser.Events
-import Element exposing (..)
+import Element exposing (Element, alignBottom, alignTop, column, el, fill, height, layout, padding, rgb255, row, spacing, text, width)
 import Element.Background as Background
-import Element.Border as Border
 import Element.Font as Font
 import Html exposing (Html)
 import Json.Decode as Decode
-import Platform.Sub as Sub
+import Platform.Sub as Sub exposing (Sub)
+import Update.Extra exposing (sequence)
 
 
 type alias Model =
-    { keyBuffer : List String
+    { keyStrokes : List String
+    , bufferContent : String
     , mode : Mode
     }
 
@@ -20,6 +21,7 @@ type alias Model =
 type Msg
     = NoOp
     | KeyDown String
+    | SetMode Mode
 
 
 type Mode
@@ -27,8 +29,12 @@ type Mode
     | Insert
 
 
+initModel : Model
 initModel =
-    { keyBuffer = [], mode = Normal }
+    { keyStrokes = []
+    , bufferContent = ""
+    , mode = Normal
+    }
 
 
 init : ( Model, Cmd Msg )
@@ -36,6 +42,7 @@ init =
     ( initModel, Cmd.none )
 
 
+sub : Sub Msg
 sub =
     Browser.Events.onKeyDown keyDecoder
         |> Sub.map KeyDown
@@ -59,8 +66,36 @@ main =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        KeyDown string ->
-            ( { model | keyBuffer = model.keyBuffer ++ [ string ] }, Cmd.none )
+        KeyDown key ->
+            let
+                setModeCmd =
+                    case ( model.mode, key ) of
+                        ( Normal, "i" ) ->
+                            Just (SetMode Insert)
+
+                        ( Insert, "Escape" ) ->
+                            Just (SetMode Normal)
+
+                        _ ->
+                            Nothing
+
+                bufferContent =
+                    if model.mode == Insert then
+                        handleInsertMode model.bufferContent key
+
+                    else
+                        model.bufferContent
+            in
+            ( { model
+                | keyStrokes = key :: model.keyStrokes
+                , bufferContent = bufferContent
+              }
+            , Cmd.none
+            )
+                |> sequence update (List.filterMap identity [ setModeCmd ])
+
+        SetMode mode ->
+            ( { model | mode = mode }, Cmd.none )
 
         NoOp ->
             ( model, Cmd.none )
@@ -70,35 +105,97 @@ view : Model -> Html Msg
 view model =
     Element.layout [ height fill, width fill ] <|
         column
-            [ width fill, height fill ]
+            [ width fill, height fill, Font.family [ Font.monospace ] ]
             [ viewBufferNames
-            , viewBuffer
+            , viewBuffer model.bufferContent
             , viewAirline model
-
-            -- , viewKeyBuffer model.keyBuffer
+            , viewKeyBuffer model.keyStrokes
             ]
 
 
+viewBufferNames : Element msg
 viewBufferNames =
-    row [] [ text "File" ]
+    row [ width fill, padding 4, Background.color (rgb255 222 222 222) ]
+        [ text "Buffer" ]
 
 
-viewBuffer =
-    column [ height fill, width fill ] [ text "Buffer" ]
+viewBuffer : String -> Element msg
+viewBuffer bufferContent =
+    let
+        lines =
+            String.lines bufferContent
+
+        lineNumbers =
+            lines
+                |> List.indexedMap (\a _ -> a)
+                |> List.map ((+) 1 >> String.fromInt >> text)
+                |> column [ alignTop, Background.color (rgb255 240 240 240) ]
+
+        bufferLines =
+            lines
+                |> List.map (emptyToSpace >> text)
+                |> column [ alignTop ]
+    in
+    row
+        [ alignTop, height fill, width fill, spacing 4 ]
+        [ lineNumbers
+        , bufferLines
+        ]
 
 
+viewAirline : Model -> Element msg
 viewAirline model =
-    row [ alignBottom ] [ text (modeToString model.mode) ]
+    row [ alignBottom, width fill, Background.color (rgb255 222 222 222) ]
+        [ el [ Background.color (rgb255 200 200 200), padding 4 ] <| text (modeToString model.mode) ]
 
 
-viewKeyBuffer keyBuffer =
-    row [ alignBottom ] <| List.map text keyBuffer
+viewKeyBuffer : List String -> Element msg
+viewKeyBuffer keyStrokes =
+    row
+        [ alignBottom, Font.size 10, padding 4 ]
+        [ text <| String.join " " keyStrokes ]
 
 
+modeToString : Mode -> String
 modeToString mode =
     case mode of
         Insert ->
-            "insert"
+            "Insert"
 
         Normal ->
             "Normal"
+
+
+handleInsertMode : String -> String -> String
+handleInsertMode buffer key =
+    case key of
+        "Enter" ->
+            buffer ++ "\n"
+
+        "Backspace" ->
+            String.dropRight 1 buffer
+
+        "Shift" ->
+            buffer
+
+        "Alt" ->
+            buffer
+
+        "Meta" ->
+            buffer
+
+        "Escape" ->
+            buffer
+
+        _ ->
+            buffer ++ key
+
+
+emptyToSpace : String -> String
+emptyToSpace s =
+    case s of
+        "" ->
+            " "
+
+        _ ->
+            s

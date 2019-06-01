0729053fc9e6da9ae06ae18780789a5b397af6e2
andys8 <andys8@users.noreply.github.com>
2019-06-01 19:12:18 +0200

    Service worker removed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import "./main.css";
 import { Elm } from "./Main.elm";
-import registerServiceWorker from "./registerServiceWorker";
 
 const app = Elm.Main.init({
   node: document.getElementById("root")
 });
-
-registerServiceWorker();

7fdd33d8f26d43d29dd3da7d3eb2372a70f652a4
andys8 <andys8@users.noreply.github.com>
2019-10-13 23:57:39 +0200

    Updates

---
+++ precache-manifest.c266d0cb1d24f82783ffd59e232d199c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5e24278cf0d3e66d199386875746ff2",
+    "url": "/vim-emulation/favicon.ico"
+  },
+  {
+    "revision": "efd65c9629ab78b06428a05a38c3251a",
+    "url": "/vim-emulation/index.html"
+  },
+  {
+    "revision": "896c0d7a0a4ac28660a78a2c6d287865",
+    "url": "/vim-emulation/manifest.json"
+  },
+  {
+    "revision": "94b73c7ec5f6e685188a",
+    "url": "/vim-emulation/static/css/main.13ef1063.chunk.css"
+  },
+  {
+    "revision": "94b73c7ec5f6e685188a",
+    "url": "/vim-emulation/static/js/main.94b73c7e.chunk.js"
+  },
+  {
+    "revision": "fdd25489a56b5d76bb0b",
+    "url": "/vim-emulation/static/js/runtime~main.fdd25489.js"
+  },
+  {
+    "revision": "3ca81432027bbb7b376c",
+    "url": "/vim-emulation/static/js/vendors~main.3ca81432.chunk.js"
+  }
+]);
